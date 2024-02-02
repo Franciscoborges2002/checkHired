73b0cd68497ea0f0426fd4146339453d66f46e56
Francisco Borges <franciscomsborges2002@gmail.com>
2024-02-02 19:23:27 +0000

    Projeto acabado

--- app/_layout.js
+++ app/_layout.js
@@ -1,27 +1,57 @@
 import { Stack } from "expo-router";
 import { useCallback } from "react";
 import { useFonts } from "expo-font";
+//import { Roboto_700Bold, Roboto_500Medium, Roboto_400Regular } from '@expo-google-fonts/roboto';
 import * as SplashScreen from "expo-splash-screen";
 
-SplashScreen.preventAutoHideAsync();
+//SplashScreen.preventAutoHideAsync();
 
 const Layout = () => {
-  /*const [fontsLoaded] = useFonts({
+  const [fontsLoaded] = useFonts({
     DMBold: require("../assets/fonts/DMSans-Bold.ttf"),
     DMMedium: require("../assets/fonts/DMSans-Medium.ttf"),
     DMRegular: require("../assets/fonts/DMSans-Regular.ttf"),
   });
 
-  const onLayoutRootView = useCallback(async () => {//Para usar este layout na=];l].;
-    
+  /*
+  let [fontsLoaded, fontError] = useFonts({
+    Roboto_700Bold,
+    Roboto_500Medium,
+    Roboto_400Regular
+  });*/
+
+  const onLayoutRootView = useCallback(async () => {
     if (fontsLoaded) {
-      await SplashScreen.hideAsync();
+      console.log("fontes carregadas.");
+      //await SplashScreen.hideAsync();
     }
-  }, [fontsLoaded]);
 
-  if (!fontsLoaded) return null;//Se as fontes nao forem carregadas*/
+    /*if(fontError){
+       console.log("errrrorrrrr.");
+      console.log(fontError)
+    }*/
+  }, [fontsLoaded]); //fontError
 
-  return <Stack />;//onLayout={onLayoutRootView}
+  /*if (!fontsLoaded && !fontError) {//Se as fontes nao forem carregadas
+    console.log("fontes nao fora carregadas. " + fontsLoaded);
+    return null;
+  }
+  ,
+      [
+        "expo-font",
+        {
+          "fonts": [
+            "node_modules/@expo-google-fonts/roboto/Roboto_700Bold.ttf", "node_modules/@expo-google-fonts/roboto/Roboto_500Medium.ttf.ttf", "node_modules/@expo-google-fonts/roboto/Roboto_400Regular.ttf.ttf"
+          ]
+        }
+      ]
+  */
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return <Stack onLayout={onLayoutRootView} />; //
 };
 
 export default Layout;
--- constants/theme.js
+++ constants/theme.js
@@ -10,6 +10,12 @@ const COLORS = {
   lightWhite: "#FAFAFC",
 };
 
+/* const FONT = {
+  regular: "Roboto_400Regular",
+  medium: "Roboto_500Medium",
+  bold: "Roboto_700Bold",
+}; */
+
 const FONT = {
   regular: "DMRegular",
   medium: "DMMedium",
